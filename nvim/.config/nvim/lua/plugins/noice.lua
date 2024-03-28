9ff8ab5c5dcfefea20e14731ae66e70a671f82a2
Muhammad Saad <github.5s32y@slmail.me>
2024-03-28 17:39:04 -0400

    Minor nvim tweaks

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -2,20 +2,19 @@ return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
 	dependencies = { "MunifTanjim/nui.nvim" },
-	config = function()
-		require("noice").setup({
-			lsp = {
-				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-				override = {
-					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-					["vim.lsp.util.stylize_markdown"] = true,
-					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-				},
+	opts = {
+		lsp = {
+			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 			},
-			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				long_message_to_split = true, -- long messages will be sent to a split
-			},
-		})
-	end,
+		},
+		presets = {
+			bottom_search = true, -- use a classic bottom cmdline for search
+			long_message_to_split = true, -- long messages will be sent to a split
+			lsp_doc_border = true, -- add a border to hover docs and signature help
+		},
+	},
 }
