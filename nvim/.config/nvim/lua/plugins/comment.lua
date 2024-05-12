c42c122f2cf05123bfb0cb98da6dea3574431057
Muhammad Saad <github.5s32y@slmail.me>
2024-05-12 15:03:22 -0400

    Nvim config bug fixes

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,5 +1,13 @@
 return {
 	"numToStr/Comment.nvim",
 	lazy = false,
-	config = true,
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		opts = { enable_autocmd = false },
+	},
+	config = function()
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+		})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.5",
+	lazy = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
