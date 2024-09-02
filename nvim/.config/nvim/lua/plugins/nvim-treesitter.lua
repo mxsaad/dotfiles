c9cc5874e5a1487e1df1697a08f4d916d971ff24
Muhammad Saad <github.5s32y@slmail.me>
2024-09-02 10:08:24 -0400

    nvim: Fix treesitter

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -4,22 +4,24 @@ return {
 		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = {
-			"c",
-			"lua",
-			"vim",
-			"vimdoc",
-			"query",
-			"typescript",
-			"javascript",
-			"html",
-			"css",
-			"tsx",
-			"python",
-		},
-		auto_install = true,
-		sync_install = false,
-		highlight = { enable = true },
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"typescript",
+				"javascript",
+				"html",
+				"css",
+				"tsx",
+				"python",
+			},
+			auto_install = true,
+			sync_install = false,
+			highlight = { enable = true },
+		})
+	end,
 }
