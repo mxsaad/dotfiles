cf60a854b3ec748f1275ccf9145a7d695cdc4393
Muhammad Saad <github.5s32y@slmail.me>
2024-09-03 15:53:57 -0400

    Fix colorscheme issue

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -46,9 +46,5 @@ vim.opt.expandtab = true
 -- Enable colors
 vim.o.termguicolors = true
 
--- Colorscheme
-vim.o.background = "dark"
-vim.cmd.colorscheme "catppuccin-mocha"
-
 -- vim: ts=2 sts=2 sw=2 et
 
--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,1 +1,9 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.o.background = "dark"
+		vim.cmd("colorscheme catppuccin-mocha")
+	end,
+}
