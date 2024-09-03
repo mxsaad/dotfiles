4eb560c3d19410b439ec7d83a950fbeb418f249f
Muhammad Saad <github.5s32y@slmail.me>
2024-09-03 15:50:10 -0400

    nvim: Add markview + telescope-undo

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,19 @@
 return {
 	"nvim-telescope/telescope.nvim",
-	tag = "0.1.5",
+	tag = "0.1.8",
 	lazy = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"nvim-treesitter/nvim-treesitter",
+		"nvim-telescope/telescope-ui-select.nvim",
+		"debugloop/telescope-undo.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		{ "nvim-telescope/telescope-ui-select.nvim" },
 	},
 	config = function()
 		require("telescope").load_extension("fzf")
 		require("telescope").load_extension("ui-select")
+		require("telescope").load_extension("undo")
 	end,
 	keys = {
 		{ "<leader>sf", "<cmd>Telescope find_files hidden=true<cr>", desc = "Search for [f]iles" },
@@ -20,5 +22,6 @@ return {
 		{ "<leader>sh", "<cmd>Telescope help_tags<cr>", desc = "Search for [h]elp" },
 		{ "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Search for [k]eymaps" },
 		{ "<leader>sc", "<cmd>Telescope commands<cr>", desc = "Search for [c]ommands" },
+		{ "<leader>su", "<cmd>Telescope undo<cr>", desc = "Search in [u]ndo tree" },
 	},
 }
