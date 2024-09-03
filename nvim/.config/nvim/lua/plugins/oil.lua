928c7c41b8fe6ced0390a387457094b2940e7ea2
Muhammad Saad <github.5s32y@slmail.me>
2024-09-03 23:36:40 +0200

    nvim: Use Oil in floating pane

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -5,5 +5,5 @@ return {
 		skip_confirm_for_simple_edits = true,
 		lsp_file_methods = { autosave_changes = true },
 	},
-	keys = { { "-", "<cmd>Oil<cr>", desc = "Open parent directory" } },
+	keys = { { "-", "<cmd>Oil --float<cr>", desc = "Open parent directory" } },
 }
